1c8992b915c07e9ba2815ff29afa457780183cf9
bhanusrimanasa <bhanusrimanasa@gmail.com>
2024-09-14 21:48:50 +0530

    Create javascript2.js

---
+++ javascript2.js
@@ -0,0 +1,114 @@
+let questions = [
+                
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+    "Hyper Text Preprocessor",
+    "Hyper Text Markup Language",
+    "Hyper Text Multiple Language",
+    "Hyper Tool Multi Language"
+    ]
+    },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+    
+    "Common Style Sheet",
+    "Colorful Style Sheet",
+    "Computer Style Sheet",
+    "Cascading Style Sheet"
+    ]
+    },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+    "Hypertext Preprocessor",
+    "Hypertext Programming",
+    "Hypertext Preprogramming",
+    "Hometext Preprocessor"
+    ]
+},
+{
+    numb: 4,
+    question: "In how many ways CSS can be written?",
+answer: "3",
+options: [
+"1",
+"2",
+"3",
+"4"
+]
+},
+{
+numb: 5,
+question: "What does XML stand for?",
+answer: "eXtensible Markup Language",
+options: [
+"eXtensible Markup Language",
+"eXecutable Multiple Language",
+"eXTra Multi-Program Language",
+"eXamine Multiple Language"
+]
+},
+{
+numb: 6,
+question: "Which of the following is a part of CSS box model?",
+answer: "All of these",
+options: [
+"Margin",
+"Borders",
+"Padding",
+"All of these"
+]
+},
+{
+numb: 7,
+question: "WWhich of the following CSS selectors are used to specify a group of elements?",
+answer: "class",
+options: [
+"tag",
+"id",
+"class",
+"tag and class"
+]
+},
+{
+numb: 8,
+question: "Which of the following CSS style property is used to specify an italic text?",
+answer: "font-style",
+options: [
+"style",
+"font-style",
+"font",
+"font-face"
+]
+},
+{
+numb: 9,
+question: "Which of the following is the CSS Extension prefixes for Webkit?",
+answer: "-webkit",
+options: [
+"-chrome",
+"-webkit",
+"-o-",
+"-web"
+]
+},
+{
+numb: 10,
+question: "Which of the following is not a property of the CSS box model?",
+answer: "color",
+options: [
+"height",
+"margin",
+"width",
+"color"
+]
+},
+];
